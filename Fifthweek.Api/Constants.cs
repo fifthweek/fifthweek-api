d65f7397eaac432b8668b218c30717c969d9d8e6
James Thurley <mail@jamesthurley.com>
2014-11-24 11:25:25 +0000

    Set Access-Control-Allow-Origin from AppSettings

--- Fifthweek.Api/Constants.cs
+++ Fifthweek.Api/Constants.cs
@@ -4,8 +4,6 @@
     {
         public const string AdministratorUsers = "Admin";
 
-        public const string FifthweekWebsiteOrigin = "http://localhost:9000";
-
         public const string DefaultAllowedOrigin = "*";
         
         public const string AllowedOriginHeaderKey = "Access-Control-Allow-Origin";
@@ -15,5 +13,12 @@
         public const string TokenRefreshTokenLifeTimeKey = "fifthweek:clientRefreshTokenLifeTime";
 
         public const string TokenClientIdKey = "fifthweek:client_id";
+
+        public static readonly string FifthweekWebsiteOrigin = GetWebsiteOrigin();
+
+        private static string GetWebsiteOrigin()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["ACCESS_CONTROL_ALLOW_ORIGIN"];
+        }
     }
 }
