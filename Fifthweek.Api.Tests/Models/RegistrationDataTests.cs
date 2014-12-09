2942ba5c4da6ecf7736a0fb5eab72a349b853c80
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-09 07:46:19 +0000

    Add equality members to RegistrationData

---
+++ Fifthweek.Api.Tests/Models/RegistrationDataTests.cs
@@ -0,0 +1,69 @@
+﻿using Fifthweek.Api.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fifthweek.Api.Tests.Models
+{
+    [TestClass]
+    public class RegistrationDataTests
+    {
+        [TestMethod]
+        public void ItShouldRecogniseEqualObjects()
+        {
+            var registration1 = NewRegistrationData();
+            var registration2 = NewRegistrationData();
+
+            Assert.AreEqual(registration1, registration2);
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentxampleWork()
+        {
+            var registration1 = NewRegistrationData();
+            var registration2 = NewRegistrationData();
+            registration2.ExampleWork = "Different";
+
+            Assert.AreNotEqual(registration1, registration2);
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentEmail()
+        {
+            var registration1 = NewRegistrationData();
+            var registration2 = NewRegistrationData();
+            registration2.Email = "Different";
+
+            Assert.AreNotEqual(registration1, registration2);
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentUsernamel()
+        {
+            var registration1 = NewRegistrationData();
+            var registration2 = NewRegistrationData();
+            registration2.Username = "Different";
+
+            Assert.AreNotEqual(registration1, registration2);
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentPassword()
+        {
+            var registration1 = NewRegistrationData();
+            var registration2 = NewRegistrationData();
+            registration2.Password = "Different";
+
+            Assert.AreNotEqual(registration1, registration2);
+        }
+
+        private static RegistrationData NewRegistrationData()
+        {
+            return new RegistrationData
+            {
+                ExampleWork = "TestExampleWork",
+                Email = "test@test.com",
+                Username = "TestUsername",
+                Password = "TestPassword"
+            };
+        }
+    }
+}
--- Fifthweek.Api/Models/RegistrationData.cs
+++ Fifthweek.Api/Models/RegistrationData.cs
@@ -1,4 +1,6 @@
-﻿namespace Fifthweek.Api.Models
+﻿using System;
+
+namespace Fifthweek.Api.Models
 {
     using System.ComponentModel.DataAnnotations;
 
@@ -24,5 +26,42 @@
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
+
+        protected bool Equals(RegistrationData other)
+        {
+            return string.Equals(this.ExampleWork, other.ExampleWork) && 
+                string.Equals(this.Email, other.Email) && 
+                string.Equals(this.Username, other.Username) && 
+                string.Equals(this.Password, other.Password);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((RegistrationData) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (this.ExampleWork != null ? this.ExampleWork.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (this.Email != null ? this.Email.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (this.Username != null ? this.Username.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (this.Password != null ? this.Password.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
