f45cc8a6753f237cdd9f0809d889d0bcbd02a5e9
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-09 07:55:22 +0000

    Add equality members to RegisterUserCommand

---
+++ Fifthweek.Api.Tests/Commands/RegisterUserCommandTest.cs
@@ -0,0 +1,34 @@
+﻿using Fifthweek.Api.Commands;
+using Fifthweek.Api.Tests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fifthweek.Api.Tests.Commands
+{
+    [TestClass]
+    public class RegisterUserCommandTest
+    {
+        [TestMethod]
+        public void ItShouldRecogniseEqualObjects()
+        {
+            var registration1 = NewRegisterUserCommand();
+            var registration2 = NewRegisterUserCommand();
+
+            Assert.AreEqual(registration1, registration2);
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentRegistrationData()
+        {
+            var registration1 = NewRegisterUserCommand();
+            var registration2 = NewRegisterUserCommand();
+            registration2.RegistrationData.ExampleWork = "Different";
+
+            Assert.AreNotEqual(registration1, registration2);
+        }
+
+        public static RegisterUserCommand NewRegisterUserCommand()
+        {
+            return new RegisterUserCommand(RegistrationDataTests.NewRegistrationData());
+        }
+    }
+}
--- Fifthweek.Api/Commands/RegisterUserCommand.cs
+++ Fifthweek.Api/Commands/RegisterUserCommand.cs
@@ -10,5 +10,32 @@
         }
 
         public RegistrationData RegistrationData { get; private set; }
+
+        protected bool Equals(RegisterUserCommand other)
+        {
+            return Equals(this.RegistrationData, other.RegistrationData);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((RegisterUserCommand) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.RegistrationData != null ? this.RegistrationData.GetHashCode() : 0);
+        }
     }
 }
