fa9869e0344c65da29afdaa518a70082cfe36e53
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-09 08:25:23 +0000

    Remove unused imports

--- Fifthweek.Api/ConvertExceptionsToResponsesAttribute.cs
+++ Fifthweek.Api/ConvertExceptionsToResponsesAttribute.cs
@@ -1,13 +1,8 @@
 ﻿namespace Fifthweek.Api
 {
-    using System;
-    using System.Net;
-    using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
     using System.Web.Http.Filters;
-
-    using Fifthweek.Api.Services;
 
     public class ConvertExceptionsToResponsesAttribute : ActionFilterAttribute
     {
