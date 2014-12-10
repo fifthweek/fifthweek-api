3f5558fc3c6d85f36a856410cbe5421ee9349c20
James Thurley <mail@jamesthurley.com>
2014-12-10 14:59:45 +0000

    Added Microsoft.System.WebApi.Tracing package

--- Fifthweek.Api/App_Start/WebApiConfig.cs
+++ Fifthweek.Api/App_Start/WebApiConfig.cs
@@ -11,6 +11,7 @@
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableSystemDiagnosticsTracing();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
