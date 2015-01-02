60bc7d55a9bbd3fda55b94c5ed2c187904fce10f
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-01-02 13:22:39 +0000

    Add reset password request command

---
+++ Fifthweek.Api.Identity.Tests/Membership/Commands/PasswordResetRequestCommand.cs
@@ -0,0 +1,41 @@
+﻿namespace Fifthweek.Api.Identity.Tests.Membership.Commands
+{
+    using Fifthweek.Api.Identity.Membership.Commands;
+    using Fifthweek.Api.Identity.Membership.Controllers;
+    using Fifthweek.Api.Identity.Tests.Membership.Controllers;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PasswordResetRequestCommandTests
+    {
+        [TestMethod]
+        public void ItShouldRecogniseEqualObjects()
+        {
+            var registrationData = PasswordResetRequestDataTests.NewPasswordResetRequestData();
+            var command1 = NewPasswordResetRequestCommand(registrationData);
+            var command2 = NewPasswordResetRequestCommand(registrationData);
+
+            Assert.AreEqual(command1, command2);
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentRegistrationData()
+        {
+            var registrationData = PasswordResetRequestDataTests.NewPasswordResetRequestData();
+            var command1 = NewPasswordResetRequestCommand(registrationData);
+
+            registrationData.Username = "Different";
+            var command2 = NewPasswordResetRequestCommand(registrationData);
+
+            Assert.AreNotEqual(command1, command2);
+        }
+
+        public static PasswordResetRequestCommand NewPasswordResetRequestCommand(PasswordResetRequestData registrationData)
+        {
+            return new PasswordResetRequestCommand(
+                registrationData.Email,
+                registrationData.Username);
+        }
+    }
+}
---
+++ Fifthweek.Api.Identity/Membership/Commands/PasswordResetRequestCommand.cs
@@ -0,0 +1,51 @@
+﻿namespace Fifthweek.Api.Identity.Membership.Commands
+{
+    public class PasswordResetRequestCommand
+    {
+        public PasswordResetRequestCommand(string email, string username)
+        {
+            this.Email = email;
+            this.Username = username;
+        }
+
+        public string Email { get; private set; }
+
+        public string Username { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((PasswordResetRequestCommand)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = this.Email != null ? this.Email.GetHashCode() : 0;
+                hashCode = (hashCode * 397) ^ (this.Username != null ? this.Username.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        protected bool Equals(PasswordResetRequestCommand other)
+        {
+            return string.Equals(this.Email, other.Email) && 
+                string.Equals(this.Username, other.Username);
+        }
+    }
+}
