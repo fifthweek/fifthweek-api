f6ab8e89216ba45b84214f80ef60339e5c34733c
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-01-02 18:05:00 +0000

    Add email type

---
+++ Fifthweek.Api.Identity.Tests/Membership/EmailTests.cs
@@ -0,0 +1,113 @@
+﻿using Fifthweek.Api.Identity.Membership;
+
+namespace Fifthweek.Api.Identity.Tests.Membership
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EmailTests
+    {
+        [TestMethod]
+        public void ItShouldAllowBasicEmailAddresses()
+        {
+            GoodEmail("joe@bloggs.com");
+        }
+
+        [TestMethod]
+        public void ItShouldAllowTokens()
+        {
+            GoodEmail("joe+token@bloggs.com");
+        }
+
+        [TestMethod]
+        public void ItShouldAllowSmallTlds()
+        {
+            GoodEmail("joe@bloggs.co");
+        }
+
+        [TestMethod]
+        public void ItShouldAllowLargeTlds()
+        {
+            GoodEmail("joe@bloggs.museum");
+        }
+
+        [TestMethod]
+        public void ItShouldAllowIPAddresses()
+        {
+            GoodEmail("joe@[127.0.0.1]");
+            GoodEmail("joe@127.0.0.1");
+        }
+
+        [TestMethod]
+        public void ItShouldAllowSubdomains()
+        {
+            GoodEmail("joe@sub.bloggs.com");
+        }
+
+        [TestMethod]
+        public void ItShouldAllowNumbers()
+        {
+            GoodEmail("joe123@bloggs456.com");
+        }
+
+        [TestMethod]
+        public void ItShouldNotAllowQuotedNames()
+        {
+            BadEmail("\"joe bloggs\"@bloggs.com");
+        }
+
+        [TestMethod]
+        public void ItShouldNotAllowWhitespace()
+        {
+            BadEmail(" joe@bloggs.com");
+            BadEmail("joe@bloggs.com ");
+            BadEmail("joe @bloggs.com");
+            BadEmail("joe@ bloggs.com");
+            BadEmail("jo e@bloggs.com");
+            BadEmail("joe@blo ggs.com");
+            BadEmail("joe@bloggs .com");
+            BadEmail("joe@bloggs. com");
+        }
+
+        [TestMethod]
+        public void ItShouldNotAllowUppercase()
+        {
+            BadEmail("Joe@Bloggs.com");
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseEqualObjects()
+        {
+            var email1 = Email.Parse("joe@example.com");
+            var email2 = Email.Parse("joe@example.com");
+
+            Assert.AreEqual(email1, email2);
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentObjects()
+        {
+            var email1 = Email.Parse("joe@example.com");
+            var email2 = Email.Parse("bloggs@example.com");
+
+            Assert.AreNotEqual(email1, email2);
+        }
+
+        private static void GoodEmail(string emailValue)
+        {
+            Email email;
+            var emailValid = Email.TryParse(emailValue, out email);
+
+            Assert.IsTrue(emailValid);
+            Assert.AreEqual(emailValue, email.Value);
+        }
+
+        private static void BadEmail(string emailValue)
+        {
+            Email email;
+            var emailValid = Email.TryParse(emailValue, out email);
+
+            Assert.IsFalse(emailValid);
+        }
+    }
+}
---
+++ Fifthweek.Api.Identity/Membership/Email.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+
+namespace Fifthweek.Api.Identity.Membership
+{
+    public class Email
+    {
+        private Email()
+        {
+        }
+
+        public string Value { get; private set; }
+
+        protected bool Equals(Email other)
+        {
+            return string.Equals(this.Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((Email) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.Value != null ? this.Value.GetHashCode() : 0);
+        }
+
+        public static Email Parse(string value)
+        {
+            Email retval;
+            if (!TryParse(value, out retval))
+            {
+                throw new ArgumentException("Invalid email address", value);
+            }
+
+            return retval;
+        }
+
+        public static bool TryParse(string value, out Email email)
+        {
+            try
+            {
+                new MailAddress(value);
+            }
+            catch
+            {
+                email = null;
+                return false;
+            }
+
+            if (value.Contains("\""))
+            {
+                // Quoted names are valid, but to keep things sane we're not accepting them.
+                email = null;
+                return false;
+            }
+
+            if (value.Any(c => char.IsUpper(c) || char.IsWhiteSpace(c)))
+            {
+                // Email addresses must be normalised to trimmed lowercase.
+                email = null;
+                return false;
+            }
+
+            email = new Email
+            {
+                Value = value
+            };
+
+            return true;
+        }
+    }
+}
