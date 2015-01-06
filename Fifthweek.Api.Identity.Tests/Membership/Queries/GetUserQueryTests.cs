e8419aaedb8153af03155fc42a4b0bc7d6e5cf09
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-01-06 18:01:13 +0000

    Test for optional parameters

---
+++ Fifthweek.Api.Identity.Tests/Membership/Queries/GetUserQueryTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Fifthweek.Api.Identity.Membership;
+using Fifthweek.Api.Tests.Shared;
+
+namespace Fifthweek.Api.Identity.Tests.Membership.Queries
+{
+    using Fifthweek.Api.Identity.Membership.Queries;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class GetUserQueryTests : ImmutableComplexTypeTests<GetUserQuery, GetUserQueryTests.Builder>
+    {
+        [TestMethod]
+        public void ItShouldRecogniseEquality()
+        {
+            this.TestEquality();
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentUsernames()
+        {
+            this.AssertDifference(_ => _.Username = NormalizedUsername.Parse("different"));
+        }
+
+        [TestMethod]
+        public void ItShouldRecogniseDifferentPasswords()
+        {
+            this.AssertDifference(_ => _.Password = Password.Parse("different"));
+        }
+
+        [TestMethod]
+        public void ItShouldRequireUsername()
+        {
+            this.AssertRequired(_ => _.Username = null);
+        }
+
+        [TestMethod]
+        public void ItShouldRequirePassword()
+        {
+            this.AssertRequired(_ => _.Password = null);
+        }
+
+        protected override Builder NewInstanceOfBuilderForObjectA()
+        {
+            return new Builder
+            {
+                Username = NormalizedUsername.Parse("joebloggs"),
+                Password = Password.Parse("password")
+            };
+        }
+
+        protected override GetUserQuery FromBuilder(Builder builder)
+        {
+            return new GetUserQuery(builder.Username, builder.Password);
+        }
+
+        public class Builder
+        {
+            public NormalizedUsername Username;
+            public Password Password;
+        }
+    }
+}
--- Fifthweek.Api.Identity/Membership/Queries/GetUserQuery.cs
+++ Fifthweek.Api.Identity/Membership/Queries/GetUserQuery.cs
@@ -1,4 +1,6 @@
-﻿namespace Fifthweek.Api.Identity.Membership.Queries
+﻿using System;
+
+namespace Fifthweek.Api.Identity.Membership.Queries
 {
     using Fifthweek.Api.Core;
     using Fifthweek.Api.Persistence;
@@ -8,6 +10,16 @@
     {
         public GetUserQuery(NormalizedUsername username, Password password)
         {
+            if (username == null)
+            {
+                throw new ArgumentNullException("username");
+            }
+
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+
             this.Username = username;
             this.Password = password;
         }
@@ -15,5 +27,35 @@
         public NormalizedUsername Username { get; private set; }
 
         public Password Password { get; private set; }
+
+        protected bool Equals(GetUserQuery other)
+        {
+            return Equals(this.Username, other.Username) && Equals(this.Password, other.Password);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((GetUserQuery) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((this.Username != null ? this.Username.GetHashCode() : 0)*397) ^ (this.Password != null ? this.Password.GetHashCode() : 0);
+            }
+        }
     }
 }
