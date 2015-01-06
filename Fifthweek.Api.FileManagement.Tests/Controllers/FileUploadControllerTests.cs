36fe2348490cd16ff6e50ba781e22a09e4247352
James Thurley <mail@jamesthurley.com>
2015-01-06 18:41:00 +0000

    Start writing tests for file management

---
+++ Fifthweek.Api.FileManagement.Tests/Controllers/FileUploadControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fifthweek.Api.FileManagement.Tests.Controllers
+{
+    using Fifthweek.Api.Core;
+    using Fifthweek.Api.FileManagement.Commands;
+    using Fifthweek.Api.FileManagement.Controllers;
+    using Fifthweek.Api.FileManagement.Queries;
+
+    using Moq;
+
+    [TestClass]
+    public class FileUploadControllerTests
+    {
+        [TestMethod]
+        public void WhenAnUploadRequestIsPosted_ItShouldCreateStorageAndReturnSasUriIfValid()
+        {
+            ////this.guidCreator.Setup(v => v.CreateSqlSequential()).Returns(this.guid).Verifiable();
+
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.guidCreator = new Mock<IGuidCreator>();
+            this.initiateFileUploadRequest = new Mock<ICommandHandler<InitiateFileUploadRequestCommand>>();
+            this.getSharedAccessSignatureUri = new Mock<IQueryHandler<GetSharedAccessSignatureUriQuery, string>>();
+            this.fileUploadComplete = new Mock<ICommandHandler<FileUploadCompleteCommand>>();
+
+            this.fileUploadController = new FileUploadController(
+                this.guidCreator.Object,
+                this.initiateFileUploadRequest.Object,
+                this.getSharedAccessSignatureUri.Object,
+                this.fileUploadComplete.Object);
+        }
+
+        private readonly Guid guid = Guid.NewGuid();
+
+        private Mock<IGuidCreator> guidCreator;
+
+        private Mock<ICommandHandler<InitiateFileUploadRequestCommand>> initiateFileUploadRequest;
+
+        private Mock<IQueryHandler<GetSharedAccessSignatureUriQuery, string>> getSharedAccessSignatureUri;
+
+        private Mock<ICommandHandler<FileUploadCompleteCommand>> fileUploadComplete;
+
+        private FileUploadController fileUploadController;
+    }
+}
---
+++ Fifthweek.Api.FileManagement.Tests/FileIdTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Fifthweek.Api.FileManagement.Tests
+{
+    using System;
+
+    using Fifthweek.Api.Tests.Shared;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FileIdTests : PrimitiveEqualityTests<FileId>
+    {
+        [TestMethod]
+        public void ItShouldRecogniseEquality()
+        {
+            this.TestEquality();
+        }
+
+        protected override FileId NewInstanceOfObjectA()
+        {
+            return new FileId(Guid.Parse("{EA7D04CC-803D-4338-BDC0-18D143F8FEC3}"));
+        }
+
+        protected override FileId NewInstanceOfObjectB()
+        {
+            return new FileId(Guid.Parse("{69BD9D77-D578-45A5-BC40-266653B43EEE}"));
+        }
+    }
+}
