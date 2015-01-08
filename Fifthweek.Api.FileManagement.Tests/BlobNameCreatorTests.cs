a1c57095e4d2741f614366dbc215364c45821576
James Thurley <mail@jamesthurley.com>
2015-01-08 13:30:03 +0000

    Unit tests for BlobNameCreator

---
+++ Fifthweek.Api.FileManagement.Tests/BlobNameCreatorTests.cs
@@ -0,0 +1,76 @@
+﻿namespace Fifthweek.Api.FileManagement.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BlobNameCreatorTests
+    {
+        [TestMethod]
+        public void ItShouldGenerateBase32Characters()
+        {
+            var blobNameCreator = new BlobNameCreator();
+
+            var chars = new HashSet<char>();
+            for (int i = 0; i < 32; i++)
+            {
+                var c = blobNameCreator.GenerateRandomCharacter(i);
+                Assert.IsTrue((c >= 'a' && c <= 'z') || (c >= '2' && c <= '7'));
+                Assert.IsFalse(chars.Contains(c));
+                chars.Add(c);
+            }
+
+            try
+            {
+                blobNameCreator.GenerateRandomCharacter(-1);
+                Assert.Fail("An exception should be thrown.");
+            }
+            catch (Exception t)
+            {
+                Assert.IsInstanceOfType(t, typeof(ArgumentOutOfRangeException));
+            }
+
+            try
+            {
+                blobNameCreator.GenerateRandomCharacter(32);
+                Assert.Fail("An exception should be thrown.");
+            }
+            catch (Exception t)
+            {
+                Assert.IsInstanceOfType(t, typeof(ArgumentOutOfRangeException));
+            }
+        }
+
+        [TestMethod]
+        public void ItShouldGenerateStringsOfTheCorrectLength()
+        {
+            var blobNameCreator = new BlobNameCreator();
+
+            for (int i = 0; i < 30; i++)
+            {
+                var sb = new StringBuilder();
+                blobNameCreator.AppendRandomString(sb, i);
+                Assert.AreEqual(i, sb.Length);
+            }
+        }
+
+        [TestMethod]
+        public void ItShouldGenerateUniqueNames()
+        {
+            var blobNameCreator = new BlobNameCreator();
+
+            var names = new HashSet<string>();
+            for (int i = 0; i < 100000; i++)
+            {
+                var name = blobNameCreator.CreateFileName();
+                Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+                Assert.IsTrue(name == name.Trim());
+                Assert.IsFalse(names.Contains(name));
+                names.Add(name);
+            }
+        }
+    }
+}
--- Fifthweek.Api.FileManagement/BlobNameCreator.cs
+++ Fifthweek.Api.FileManagement/BlobNameCreator.cs
@@ -1,6 +1,7 @@
 ﻿namespace Fifthweek.Api.FileManagement
 {
     using System;
+    using System.Text;
 
     public class BlobNameCreator : IBlobNameCreator
     {
@@ -8,10 +9,38 @@
 
         public string CreateFileName()
         {
-            var randomBytes = new byte[4];
-            Random.NextBytes(randomBytes);
-            var uniqifier = string.Join(string.Empty, randomBytes);
-            return DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss-ffffff") + "-" + uniqifier;
+            var sb = new StringBuilder();
+            sb.Append(DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss-fff"));
+            sb.Append('-');
+
+            this.AppendRandomString(sb, 6);
+         
+            return sb.ToString();
+        }
+
+        internal void AppendRandomString(StringBuilder sb, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(this.GenerateRandomCharacter(Random.Next(32)));
+            }
+        }
+
+        internal char GenerateRandomCharacter(int value)
+        {
+            if (value < 0 || value > 31)
+            {
+                throw new ArgumentOutOfRangeException("value");
+            }
+
+            char result = (char)(value + 'a');
+
+            if (result > 'z')
+            {
+                result = (char)(result - 'z' + '1');
+            }
+
+            return result;
         }
     }
 }
