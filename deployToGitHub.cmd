635e80fd907f8a49b290c48c27a6552f8b9b55e2
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-01-09 19:22:51 +0000

    Changed to recursive copy

--- deployToGitHub.cmd
+++ deployToGitHub.cmd
@@ -9,17 +9,17 @@ echo Cloning fifthweek-api-dist
 git clone --branch=master https://%git_personal_access_token%:x-oauth-basic@github.com/fifthweek/fifthweek-api-dist.git "%APPVEYOR_BUILD_FOLDER%\temp_dist"
 
 echo Preserving GIT settings for dist repo
-xcopy /h "%APPVEYOR_BUILD_FOLDER%\temp_dist\.git" "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings\.git\"
+xcopy /e /h "%APPVEYOR_BUILD_FOLDER%\temp_dist\.git" "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings\.git\"
 
 echo Clearing files
 rmdir /s /q "%APPVEYOR_BUILD_FOLDER%\temp_dist"
 mkdir "%APPVEYOR_BUILD_FOLDER%\temp_dist"
 
 echo Copying new files to dist repo
-xcopy /h "%APPVEYOR_BUILD_FOLDER%\dist" "%APPVEYOR_BUILD_FOLDER%\temp_dist\"
+xcopy /e /h "%APPVEYOR_BUILD_FOLDER%\dist" "%APPVEYOR_BUILD_FOLDER%\temp_dist\"
 
 echo Re-apply GIT settings for dist repo
-xcopy /h "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings\.git" "%APPVEYOR_BUILD_FOLDER%\temp_dist\.git\"
+xcopy /e /h "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings\.git" "%APPVEYOR_BUILD_FOLDER%\temp_dist\.git\"
 
 echo Changing directory
 cd "%APPVEYOR_BUILD_FOLDER%\temp_dist"
