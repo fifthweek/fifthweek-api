3d120f3e9f284e648838dcce8fb0ea2924a75816
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-01-09 18:15:30 +0000

    Fix directory deletion

--- deployToGitHub.cmd
+++ deployToGitHub.cmd
@@ -8,23 +8,18 @@ mkdir "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings"
 echo Cloning fifthweek-api-dist
 git clone --branch=master https://%git_personal_access_token%:x-oauth-basic@github.com/fifthweek/fifthweek-api-dist.git "%APPVEYOR_BUILD_FOLDER%\temp_dist"
 
-cd "%APPVEYOR_BUILD_FOLDER%"
-dir
-cd "%APPVEYOR_BUILD_FOLDER%\temp_dist"
-dir
-
 echo Preserving GIT settings for dist repo
-xcopy /h "%APPVEYOR_BUILD_FOLDER%\temp_dist\.git*" "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings"
+xcopy /h "%APPVEYOR_BUILD_FOLDER%\temp_dist\.git" "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings\.git\"
 
 echo Clearing files
-rmdir /s "%APPVEYOR_BUILD_FOLDER%\temp_dist*"
+rmdir /s /q "%APPVEYOR_BUILD_FOLDER%\temp_dist*"
 mkdir "%APPVEYOR_BUILD_FOLDER%\temp_dist"
 
 echo Copying new files to dist repo
-copy -rf "%APPVEYOR_BUILD_FOLDER%\dist\*" "%APPVEYOR_BUILD_FOLDER%\temp_dist\"
+xcopy /h "%APPVEYOR_BUILD_FOLDER%\dist" "%APPVEYOR_BUILD_FOLDER%\temp_dist\"
 
 echo Re-apply GIT settings for dist repo
-xcopy /h "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings\.git*" "%APPVEYOR_BUILD_FOLDER%\temp_dist"
+xcopy /h "%APPVEYOR_BUILD_FOLDER%\temp_dist_git_settings\.git" "%APPVEYOR_BUILD_FOLDER%\temp_dist\.git\"
 
 echo Changing directory
 cd "%APPVEYOR_BUILD_FOLDER%\temp_dist"
