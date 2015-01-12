7ac053cd937cb09cc643597505dfbc0e2d8d33ed
James Thurley <mail@jamesthurley.com>
2015-01-12 19:18:43 +0000

    Create EncodeGuid and DecodeGuid extension methods

--- Fifthweek.Api.Core/Extensions.cs
+++ Fifthweek.Api.Core/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿namespace Fifthweek.Api.Core
 {
+    using System;
+    using System.Web;
     using System.Web.Http.Dependencies;
 
     public static class Extensions
@@ -7,6 +9,28 @@
         public static TResult GetService<TResult>(this IDependencyResolver resolver)
         {
             return (TResult)resolver.GetService(typeof(TResult));
+        }
+
+        public static string EncodeGuid(this Guid value)
+        {
+            return HttpServerUtility.UrlTokenEncode(value.ToByteArray());
+        }
+
+        public static Guid DecodeGuid(this string value)
+        {
+            var result = HttpServerUtility.UrlTokenDecode(value);
+
+            if (result == null)
+            {
+                throw new ArgumentException("Value does not represent a byte stream: " + value);
+            }
+
+            if (result.Length != 16)
+            {
+                throw new ArgumentException("Value does not represent a GUID: " + value);
+            }
+
+            return new Guid(result);
         }
     }
 }
