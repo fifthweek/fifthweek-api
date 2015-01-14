49edc71e8804782dd6da879d207237ce89180986
James Thurley <mail@jamesthurley.com>
2015-01-14 20:35:21 +0000

    Add thumbnails webjob projects

---
+++ Fifthweek.Webjobs.Thumbnails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifthweek.Webjobs.Thumbnails
+{
+    using Microsoft.Azure.WebJobs;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new JobHost();
+            host.RunAndBlock();
+        }
+    }
+}
