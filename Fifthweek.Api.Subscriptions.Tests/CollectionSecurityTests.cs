d664c813623199c9a412f352af76821cb1d40d2a
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-01-16 16:12:46 +0000

    Test collection security

---
+++ Fifthweek.Api.Subscriptions.Tests/CollectionSecurityTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Fifthweek.Api.Subscriptions.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Fifthweek.Api.Core;
+    using Fifthweek.Api.Identity.Membership;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class CollectionSecurityTests
+    {
+        private static readonly UserId UserId = new UserId(Guid.NewGuid());
+        private static readonly CollectionId CollectionId = new CollectionId(Guid.NewGuid());
+        private Mock<ICollectionOwnership> collectionOwnership;
+        private CollectionSecurity target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.collectionOwnership = new Mock<ICollectionOwnership>();
+            this.target = new CollectionSecurity(this.collectionOwnership.Object);
+        }
+
+        [TestMethod]
+        public async Task WhenAuthorizingPost_ItShouldAllowIfUserOwnsCollection()
+        {
+            this.collectionOwnership.Setup(_ => _.IsOwnerAsync(UserId, CollectionId)).ReturnsAsync(true);
+
+            var result = await this.target.IsPostingAllowedAsync(UserId, CollectionId);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task WhenAuthorizingPost_ItShouldForbidIfUserDoesNotOwnCollection()
+        {
+            this.collectionOwnership.Setup(_ => _.IsOwnerAsync(UserId, CollectionId)).ReturnsAsync(false);
+
+            var result = await this.target.IsPostingAllowedAsync(UserId, CollectionId);
+
+            Assert.IsFalse(result);
+
+            try
+            {
+                await this.target.AssertPostingAllowedAsync(UserId, CollectionId);
+                Assert.Fail("Expected unauthorized exception");
+            }
+            catch (UnauthorizedException)
+            {
+            }
+        }
+    }
+}
