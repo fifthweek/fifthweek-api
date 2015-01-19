d62e892c6c65f2a142920217bed63458e1b8c2bd
James Thurley <mail@jamesthurley.com>
2015-01-19 17:56:55 +0000

    Add ThumbnailFileTask test

--- Fifthweek.WebJobs.Thumbnails.Tests/ThumbnailFileTaskTests.cs
+++ Fifthweek.WebJobs.Thumbnails.Tests/ThumbnailFileTaskTests.cs
@@ -22,19 +22,16 @@
             "purpose",
             true);
 
-        private ThumbnailFileTask target;
-
         private Mock<ICloudQueue> cloudQueue;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            target = new ThumbnailFileTask(1000, 1000, ResizeBehaviour.MaintainAspectRatio);
             this.cloudQueue = new Mock<ICloudQueue>(MockBehavior.Strict);
         }
-        
+
         [TestMethod]
-        public async Task WhenHandleAsyncCalled()
+        public async Task WhenMaintainingAspectRatio_TestHandleAsync()
         {
             CloudQueueMessage output = null;
 
@@ -42,16 +39,40 @@
                 .Callback<CloudQueueMessage>(v => output = v)
                 .Returns(Task.FromResult(0));
 
-            await this.target.HandleAsync(this.cloudQueue.Object, this.message);
+            var target = new ThumbnailFileTask(1000, 1000, ResizeBehaviour.MaintainAspectRatio);
+            await target.HandleAsync(this.cloudQueue.Object, this.message);
 
             var outputMessage = JsonConvert.DeserializeObject<CreateThumbnailMessage>(output.AsString);
 
             Assert.AreEqual(this.message.ContainerName, outputMessage.ContainerName);
             Assert.AreEqual(this.message.BlobName, outputMessage.InputBlobName);
-            Assert.AreEqual(string.Format("{0}-{1}-{2}", this.message.BlobName, this.target.Width, this.target.Height), outputMessage.OutputBlobName);
-            Assert.AreEqual(this.target.Width, outputMessage.Width);
-            Assert.AreEqual(this.target.Height, outputMessage.Height);
-            Assert.AreEqual(this.target.ResizeBehaviour, outputMessage.ResizeBehaviour);
+            Assert.AreEqual(string.Format("{0}-{1}-{2}", this.message.BlobName, target.Width, target.Height), outputMessage.OutputBlobName);
+            Assert.AreEqual(target.Width, outputMessage.Width);
+            Assert.AreEqual(target.Height, outputMessage.Height);
+            Assert.AreEqual(target.ResizeBehaviour, outputMessage.ResizeBehaviour);
+            Assert.AreEqual(this.message.Overwrite, outputMessage.Overwrite);
+        }
+
+        [TestMethod]
+        public async Task WhenCropping_TestHandleAsync()
+        {
+            CloudQueueMessage output = null;
+
+            this.cloudQueue.Setup(v => v.AddMessageAsync(It.IsAny<CloudQueueMessage>()))
+                .Callback<CloudQueueMessage>(v => output = v)
+                .Returns(Task.FromResult(0));
+
+            var target = new ThumbnailFileTask(1000, 1000, ResizeBehaviour.CropToAspectRatio);
+            await target.HandleAsync(this.cloudQueue.Object, this.message);
+
+            var outputMessage = JsonConvert.DeserializeObject<CreateThumbnailMessage>(output.AsString);
+
+            Assert.AreEqual(this.message.ContainerName, outputMessage.ContainerName);
+            Assert.AreEqual(this.message.BlobName, outputMessage.InputBlobName);
+            Assert.AreEqual(string.Format("{0}-{1}-{2}-crop", this.message.BlobName, target.Width, target.Height), outputMessage.OutputBlobName);
+            Assert.AreEqual(target.Width, outputMessage.Width);
+            Assert.AreEqual(target.Height, outputMessage.Height);
+            Assert.AreEqual(target.ResizeBehaviour, outputMessage.ResizeBehaviour);
             Assert.AreEqual(this.message.Overwrite, outputMessage.Overwrite);
         }
     }
