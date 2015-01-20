46b28862b95a3603468b37871f7af43979de59cb
James Thurley <mail@jamesthurley.com>
2015-01-20 15:41:40 +0000

    Change deletions webjob project to console app

---
+++ Fifthweek.WebJobs.Deletions/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Fifthweek.WebJobs.Deletions
+{
+    using System;
+
+    using Fifthweek.Azure;
+
+    using Microsoft.Azure.WebJobs;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new JobHostConfiguration();
+            config.DashboardConnectionString = config.StorageConnectionString = AzureConfiguration.GetStorageConnectionString();
+            config.Queues.BatchSize = 8;
+            config.Queues.MaxDequeueCount = 3;
+            config.Queues.MaxPollingInterval = TimeSpan.FromSeconds(60);
+            var host = new JobHost(config);
+            host.RunAndBlock();
+        }
+    }
+}
