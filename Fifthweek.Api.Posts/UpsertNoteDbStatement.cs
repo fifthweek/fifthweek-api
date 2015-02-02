a81522ab460cd60838c6aec75f672ad49c169988
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-02 22:06:46 +0000

    Fix upsert note statement

--- Fifthweek.Api.Posts/UpsertNoteDbStatement.cs
+++ Fifthweek.Api.Posts/UpsertNoteDbStatement.cs
@@ -42,7 +42,7 @@ namespace Fifthweek.Api.Posts
                 note.Value,
                 false,
                 liveDate,
-                default(DateTime));
+                now);
 
             if (isNewPost)
             {
