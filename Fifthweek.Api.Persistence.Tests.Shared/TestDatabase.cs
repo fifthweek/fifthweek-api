1936f8f03c9bb597ae23a43517c7a967e6e34d8b
James Thurley <mail@jamesthurley.com>
2015-04-15 16:34:29 +0100

    Update seed database version

--- Fifthweek.Api.Persistence.Tests.Shared/TestDatabase.cs
+++ Fifthweek.Api.Persistence.Tests.Shared/TestDatabase.cs
@@ -14,7 +14,7 @@
     public partial class TestDatabase : IFifthweekDbConnectionFactory
     {
         // Increment this when changing database seed state.
-        private const int SeedStateVersion = 19;
+        private const int SeedStateVersion = 20;
 
         private static bool migrationRequired = true;
 
