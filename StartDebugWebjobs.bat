e848f5f812da59f10a1bb0be33865c0a5eedfb74
James Thurley <mail@jamesthurley.com>
2015-07-31 10:17:50 +0100

    Create batch file to start all webjobs from debug folders

---
+++ StartDebugWebjobs.bat
@@ -0,0 +1,16 @@
+
+pushd Fifthweek.WebJobs.Thumbnails\bin\Debug
+start run.exe
+popd
+
+pushd Fifthweek.WebJobs.Snapshots\bin\Debug
+start run.exe
+popd
+
+pushd Fifthweek.WebJobs.Payments\bin\Debug
+start run.exe
+popd
+
+pushd Fifthweek.WebJobs.GarbageCollection\bin\Debug
+start run.exe
+popd
